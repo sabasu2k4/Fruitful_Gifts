8e75159f7327e1afe1cadec9701ec31cfe50d913
sabasu2k4 <nguyenvietduong15082004@gmail.com>
2025-04-25 21:21:17 +0700

    Source Code New

---
+++ Fruitful-Gifts/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fruitful_Gifts.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
